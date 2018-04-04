c88d1b63fb3a962c62172614c0df8bda549d759f
leon <745370941@qq.com>
2018-04-04 17:12:44 +0800

    init demo

---
+++ demo/src/main/java/cn/zhang/opensource/demo/App.java
@@ -0,0 +1,13 @@
+package cn.zhang.opensource.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
